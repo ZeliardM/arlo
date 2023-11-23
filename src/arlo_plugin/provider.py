b8c88aca21582330b003558620ee78d1c627b639
ZeliardM <ZeliardM@gmail.com>
2023-11-22 21:21:52 -0500

    moved smss manifest to load into arlo_smss database in provider

--- src/arlo_plugin/provider.py
+++ src/arlo_plugin/provider.py
@@ -852,14 +852,6 @@ class ArloProvider(ScryptedDeviceBase, Settings, DeviceProvider, ScryptedDeviceL
                     self.logger.info(f"Skipping security mode security system {nativeId} (Arlo Security Mode Security System - {locations[location]}) as it has already been added.")
                     continue
 
-                self.arlo_smss[nativeId] = ""
-
-                if nativeId in self.hidden_device_ids:
-                    self.logger.info(f"Skipping security mode security system {nativeId} (Arlo Security Mode Security System - {locations[location]}) because it is hidden.")
-                    continue
-
-                device = await self.getDevice_impl(nativeId)
-                scrypted_interfaces = device.get_applicable_interfaces()
                 manifest = {
                     "info": {
                         "model": "Arlo Security Mode Security System",
@@ -873,6 +865,15 @@ class ArloProvider(ScryptedDeviceBase, Settings, DeviceProvider, ScryptedDeviceL
                     "type": device.get_device_type(),
                     "providerNativeId": None,
                 }
+
+                self.arlo_smss[nativeId] = manifest
+
+                if nativeId in self.hidden_device_ids:
+                    self.logger.info(f"Skipping security mode security system {nativeId} (Arlo Security Mode Security System - {locations[location]}) because it is hidden.")
+                    continue
+
+                device = await self.getDevice_impl(nativeId)
+                scrypted_interfaces = device.get_applicable_interfaces()
                 self.logger.debug(f"Interfaces for {nativeId}: {scrypted_interfaces}")
 
                 # we want to add the security mode security system as a top level device
