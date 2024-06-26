7cea0c4dfc03107696dd342b48673baba20c70bb
ZeliardM <ZeliardM@gmail.com>
2024-06-26 17:06:59 -0400

    additional logging and resource cleanup for child_process.py

--- src/arlo_plugin/child_process.py
+++ src/arlo_plugin/child_process.py
@@ -13,7 +13,7 @@ HEARTBEAT_INTERVAL = 5
 def multiprocess_main(name, logger_port, child_conn, exe, args, buffer_queue=None, binary_output=False):
     logger = scrypted_arlo_go.NewTCPLogger(logger_port, "HeartbeatChildProcess")
 
-    logger.Send(f"{name} starting\n")
+    logger.Send(f"{name} child process starting\n")
     sp = subprocess.Popen([exe, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stop_event = threading.Event()
 
@@ -34,6 +34,7 @@ def multiprocess_main(name, logger_port, child_conn, exe, args, buffer_queue=Non
     stdout_t.start()
     stderr_t.start()
 
+    logger.Send(f"{name} child process started\n")
     try:
         while True:
             has_data = child_conn.poll(HEARTBEAT_INTERVAL * 3)
@@ -45,18 +46,22 @@ def multiprocess_main(name, logger_port, child_conn, exe, args, buffer_queue=Non
             if not keep_alive:
                 break
     finally:
-        logger.Send(f"{name} exiting\n")
+        logger.Send(f"{name} child process exiting\n")
         sp.terminate()
         try:
-            sp.wait(timeout=HEARTBEAT_INTERVAL)
+            sp.wait(timeout=1)
         except subprocess.TimeoutExpired:
             sp.kill()
         stop_event.set()
 
+        sp.stdout.close()
+        sp.stderr.close()
+        sp = None
+
         stdout_t.join()
         stderr_t.join()
 
-        logger.Send(f"{name} exited\n")
+        logger.Send(f"{name} child process exited\n")
         logger.Close()
 
 
@@ -76,6 +81,9 @@ class HeartbeatChildProcess:
         self.binary_output = binary_output
         self.args = args
 
+        self.logger = scrypted_arlo_go.NewTCPLogger(logger_port, "HeartbeatParentProcess")
+        self.logger.Send(f"{name} parent process starting\n")
+
         self.parent_conn, self.child_conn = multiprocessing.Pipe()
         self.buffer_queue = multiprocessing.Queue() if self.binary_output else None
         
@@ -85,6 +93,7 @@ class HeartbeatChildProcess:
         self.process = multiprocessing.Process(target=multiprocess_main, args=(self.name, self.logger_port, self.child_conn, self.exe, self.args, self.buffer_queue, self.binary_output))
         self.process.daemon = True
         self._stop = False
+        self.logger.Send(f"{name} parent process started\n")
 
     async def start(self):
         self.process.start()
@@ -92,9 +101,15 @@ class HeartbeatChildProcess:
 
     async def stop(self):
         self._stop = True
+        self.logger.Send(f"{self.name} parent process stopping\n")
         if self.process.is_alive():
             self.parent_conn.send(False)
+            self.parent_conn.poll(1)
+            self.process.terminate()
             self.process.join()
+            if self.process.is_alive():
+                self.process.kill()
+                self.process.join()
         self.parent_conn.close()
         self.child_conn.close()
         if self.heartbeat_task:
@@ -102,14 +117,19 @@ class HeartbeatChildProcess:
         if self.binary_output:
             self.buffer_queue.close()
             self.buffer_queue.join_thread()
+        self.logger.Send(f"{self.name} parent process stopped\n")
+        self.logger.Close()
 
     async def heartbeat(self):
         try:
+            self.logger.Send(f"{self.name} heartbeat\n")
             while not self._stop:
                 await asyncio.sleep(HEARTBEAT_INTERVAL)
                 if not self.process.is_alive():
+                    self.logger.Send(f"{self.name} heartbeat not detected, stopping processes\n")
                     await self.stop()
                     break
+                self.logger.Send(f"{self.name} heartbeat detected\n")
                 self.parent_conn.send(True)
         except asyncio.CancelledError:
             pass
@@ -128,13 +148,16 @@ class HeartbeatChildProcess:
                     if not found_start:
                         start = temp_buffer.find(b'\xFF\xD8')
                         if start != -1:
+                            self.logger.Send(f"{self.name} found start of frame\n")
                             found_start = True
                             temp_buffer = temp_buffer[start:]
                     else:
                         end = temp_buffer.find(b'\xFF\xD9')
                         if end != -1:
+                            self.logger.Send(f"{self.name} found end of frame\n")
                             end += 2
                             frame = temp_buffer[:end]
+                            self.logger.Send(f"{self.name} sending frame to buffer\n")
                             self.buffer_output.append(frame)
                             break
                 except queue.Empty:
